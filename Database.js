98d2dfe31dbc444e292720d6dc4203ae3af74877
PranavMishra07 <pranav072003@gmail.com>
2025-03-06 11:48:43 +0530

    sd

---
+++ Database.js
@@ -0,0 +1,106 @@
+function appoint() {
+    let appointdata = {
+        aname: document.querySelector("#name").value,
+        aemail: document.querySelector("#email").value,
+        acontact: document.querySelector("#contact").value,
+        aservice: document.querySelector("#service").value,
+        adate: document.querySelector("#date").value,
+        atime: document.querySelector("#time").value,
+    }
+    fetch("http://localhost:3000/Doctor", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(appointdata)
+    }).then(r => alert("Appoointment Submitted"))
+}
+async function data() {
+    let data = await fetch("http://localhost:3000/Doctor")
+    let fdata = await data.json()
+    let tdata = fdata.map((e) => `
+    <tr>
+    <td>${e.id}</td>
+    <td>${e.aname}</td>
+    <td>${e.aemail}</td>
+    <td>${e.acontact}</td>
+    <td>${e.aservice}</td>
+    <td>${e.adate}</td>
+    <td>${e.atime}</td>
+    <td><button id="delbtn" onclick="mydel('${e.id}')" >delete </button></td>
+    <td><button id="edbtn" onclick="myedit('${e.id}')" >edit </button></td>
+    </tr>
+    `).join("")
+    let a = document.querySelector("#display")
+    a.innerHTML = tdata
+}
+data()
+
+
+function mydel(id) {
+    fetch(`http://localhost:3000/Doctor/${id}`, {
+        method: "DELETE"
+    })
+        .then(r => alert("deleted"))
+}
+
+
+
+async function myedit(id) {
+    let edata = await fetch(`http://localhost:3000/Doctor/${id}`)
+    let fdata = await edata.json()
+    let frm = `
+ 
+    <input type="text" value="${fdata.id}" id="id1"  ><br><br>
+    <input type="text" value="${fdata.aname}" id="name1"  ><br><br>
+    <input type="text"  value="${fdata.aemail}" id="email" ><br><br>
+    <input type="text"  value="${fdata.acontact}" id="contact1"><br><br>
+    <select  value="${fdata.aservice}" id="service1" >
+      <option value="">Select Service</option>
+      <option value="surgery">Surgery & Radiology</option>
+      <option value="neurology">Neurology</option>
+      <option value="angiography">Angiography</option>
+      <option value="children-care">Children Care</option>
+      <option value="orthopedics">Orthopedics</option>
+      <option value="nuclear-magnetic">Nuclear Magnetic</option>
+      <option value="eye-treatment">Eye Treatment</option>
+      <option value="x-ray">X-Ray</option>
+    </select>
+    <input type="date" value="${fdata.adate}" id="date1" required  ><br><br>
+   <select   value="${fdata.atime}"    id="time1" required>
+       <option>Select Time</option>
+       <option>09.00 AM - 10.00 AM</option>
+       <option>10.00 AM - 11.00 AM</option>
+       <option>11.00 AM - 12.00 PM</option>
+       <option>12.00 PM - 01.00 PM</option>
+       <option>01.00 PM - 03.00 PM</option>
+       <option>04.00 PM - 06.00 PM</option>
+       <option>07.00 PM - 10.00 PM</option>
+     </select>
+
+    
+    <input type="submit" onclick="finalupdate('${fdata.id}')">
+
+    `
+    document.querySelector("#showfrm").innerHTML = frm
+
+}
+
+function finalupdate(id) {
+    let final = {
+        id: document.querySelector("#id1").value,
+        name: document.querySelector("#name1").value,
+        email: document.querySelector("#email").value,
+        contact: document.querySelector("#contact1").value,
+        service: document.querySelector("#service1").value,
+        date: document.querySelector("#date1").value,
+        time: document.querySelector("#time1").value,
+    }
+    fetch(`http://localhost:3000/Doctor/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(final)
+    }).then(r => alert("Data updated"))
+}
--- doctor.js
+++ doctor.js
@@ -1,3 +1,6 @@
+function toggleMenu() {
+    document.querySelector(".menu").classList.toggle("active");
+}
 
 // localStorage.setItem("logindata", JSON.stringify(logindata))
 // let admincheck = JSON.parse(localStorage.getItem("logindata"))
@@ -6,11 +9,11 @@ function adminlogin() {
     let admin_email = document.querySelector("#admin_email").value
     let admin_password = document.querySelector("#admin_password").value
     if (admin_email != "pranav072003@gmail.com") {
-        alert("EmIAL IS WERONF")
+        alert("Email is wrong")
         return false
     }
     if (admin_password != "pranav@123") {
-        alert("Data Not Found")
+        alert("Wrong Password")
         return false
     }
 }
@@ -37,85 +40,85 @@ function pateintlogin() {
     }
 }
 
-function appoint() {
-    let appointdata = {
-        aname: document.querySelector("#name").value,
-        aemail: document.querySelector("#email").value,
-        acontact: document.querySelector("#contact").value,
-        aservice: document.querySelector("#service").value,
-        adate: document.querySelector("#date").value,
-        atime: document.querySelector("#time").value,
-    }
-    fetch("http://localhost:3000/Doctor", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(appointdata)
-    }).then(r => alert("Inserted"))
-}
-async function data() {
-    let data = await fetch("http://localhost:3000/Doctor")
-    let fdata = await data.json()
-    let tdata = fdata.map((e) => `
-    <tr>
-    <td>${e.id}</td>
-    <td>${e.aname}</td>
-    <td>${e.aemail}</td>
-    <td>${e.acontact}</td>
-    <td>${e.aservice}</td>
-    <td>${e.adate}</td>
-    <td>${e.atime}</td>
-    <td><button id="delbtn" onclick="mydel('${e.id}')" >delete </button></td>
-    <td><button id="edbtn" onclick="myedit('${e.id}')" >edit </button></td>
-    </tr>
-    `).join("")
-    let a = document.querySelector("#display")
-    a.innerHTML = tdata
-}
-data()
+// function appoint() {
+//     let appointdata = {
+//         aname: document.querySelector("#name").value,
+//         aemail: document.querySelector("#email").value,
+//         acontact: document.querySelector("#contact").value,
+//         aservice: document.querySelector("#service").value,
+//         adate: document.querySelector("#date").value,
+//         atime: document.querySelector("#time").value,
+//     }
+//     fetch("http://localhost:3000/Doctor", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(appointdata)
+//     }).then(r => alert("Inserted"))
+// }
+// async function data() {
+//     let data = await fetch("http://localhost:3000/Doctor")
+//     let fdata = await data.json()
+//     let tdata = fdata.map((e) => `
+//     <tr>
+//     <td>${e.id}</td>
+//     <td>${e.aname}</td>
+//     <td>${e.aemail}</td>
+//     <td>${e.acontact}</td>
+//     <td>${e.aservice}</td>
+//     <td>${e.adate}</td>
+//     <td>${e.atime}</td>
+//     <td><button id="delbtn" onclick="mydel('${e.id}')" >delete </button></td>
+//     <td><button id="edbtn" onclick="myedit('${e.id}')" >edit </button></td>
+//     </tr>
+//     `).join("")
+//     let a = document.querySelector("#display")
+//     a.innerHTML = tdata
+// }
+// data()
 
 
-function mydel(id) {
-    fetch(`http://localhost:3000/Doctor/${id}`, {
-        method: "DELETE"
-    })
-        .then(r => alert("deleted"))
-}
+// function mydel(id) {
+//     fetch(`http://localhost:3000/Doctor/${id}`, {
+//         method: "DELETE"
+//     })
+//         .then(r => alert("deleted"))
+// }
 
 
 
-async function myedit(id) {
-    let edata = await fetch(`http://localhost:3000/Doctor/${id}`)
-    let fdata = await edata.json()
-    let frm = `
-    <input type="text" value="${fdata.id}" id="id1"  ><br><br>
-    <input type="text" value="${fdata.aname}" id="name1"  ><br><br>
-    <input type="text"  value="${fdata.aemail}" id="email" ><br><br>
-    <input type="text"  value="${fdata.acontact}" id="contact1"><br><br>
-    <input type="text" value="${fdata.aservice}" id="service1"  ><br><br>
-    <input type="text" value="${fdata.adate}" id="date1"  ><br><br>
-    <input type="text" value="${fdata.atime}" id="time1"  ><br><br>
-    <input type="submit" onclick="finalupdate('${fdata.id}')">
-    `
-    document.querySelector("#showfrm").innerHTML = frm
-}
+// async function myedit(id) {
+//     let edata = await fetch(`http://localhost:3000/Doctor/${id}`)
+//     let fdata = await edata.json()
+//     let frm = `
+//     <input type="text" value="${fdata.id}" id="id1"  ><br><br>
+//     <input type="text" value="${fdata.aname}" id="name1"  ><br><br>
+//     <input type="text"  value="${fdata.aemail}" id="email" ><br><br>
+//     <input type="text"  value="${fdata.acontact}" id="contact1"><br><br>
+//     <input type="text" value="${fdata.aservice}" id="service1"  ><br><br>
+//     <input type="text" value="${fdata.adate}" id="date1"  ><br><br>
+//     <input type="text" value="${fdata.atime}" id="time1"  ><br><br>
+//     <input type="submit" onclick="finalupdate('${fdata.id}')">
+//     `
+//     document.querySelector("#showfrm").innerHTML = frm
+// }
 
-function finalupdate(id) {
-    let final = {
-        id: document.querySelector("#id1").value,
-        name: document.querySelector("#name1").value,
-        email: document.querySelector("#email").value,
-        contact: document.querySelector("#contact1").value,
-        service: document.querySelector("#service1").value,
-        date: document.querySelector("#date1").value,
-        time: document.querySelector("#time1").value,
-    }
-    fetch(`http://localhost:3000/Doctor/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(final)
-    }).then(r => alert("updated"))
-}
+// function finalupdate(id) {
+//     let final = {
+//         id: document.querySelector("#id1").value,
+//         name: document.querySelector("#name1").value,
+//         email: document.querySelector("#email").value,
+//         contact: document.querySelector("#contact1").value,
+//         service: document.querySelector("#service1").value,
+//         date: document.querySelector("#date1").value,
+//         time: document.querySelector("#time1").value,
+//     }
+//     fetch(`http://localhost:3000/Doctor/${id}`, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(final)
+//     }).then(r => alert("updated"))
+// }
